c92e69135f1fa9e473b09f1cba3a74d8df8c53c5
CheesecakeYT <89538480+CheesecakeYT@users.noreply.github.com>
2022-02-11 13:48:25 +0100

    Create AspectraTotalSecurity.py

---
+++ python_version/AspectraTotalSecurity.py
@@ -0,0 +1,5 @@
+sha256 = input('Prosime, zadejte SHA256 souboru: ')
+threat = 'Zadna hrozba'
+sha256.replace(' ', '')
+if sha256 == '13c92506e1ec7986e4ead73470f5516bf1c01f25b46989551c67c22dba76bead' threat = 'Win32/Ransom.WannaCryptor.A'
+print(threat)
